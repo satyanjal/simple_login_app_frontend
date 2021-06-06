08741adb8606ad8a267872a11d52d842dc8a505b
Satyanjal Praksah <satyanjal.prakash@analyttica.com>
2021-06-06 22:30:42 +0530

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Lobby from '../Lobby/Lobby';
+
+function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/lobby">
+            <Lobby />
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Lobby/Lobby.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+export default function Lobby() {
+
+  return(
+    <div>
+      <h1>
+      Waiting Lobby
+      </h1>
+    </div>
+  )
+}
